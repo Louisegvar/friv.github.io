bdf6c4b2866765bc62e32dd44a2cdc1a8d48852f
Louise Geppert Varela <acacianoggins@gmail.com>
2025-11-19 15:14:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Seleciona elementos
+const form = document.getElementById('cadastroForm');
+const mensagem = document.getElementById('mensagemSucesso');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // evita recarregar a página
+
+    // Mostrar a mensagem de sucesso
+    mensagem.classList.add('mostrar');
+
+    // Limpar o formulário
+    form.reset();
+
+    // Esconder a mensagem após 5 segundos
+    setTimeout(() => {
+        mensagem.classList.remove('mostrar');
+    }, 5000);
+    setTimeout(function() {
+    window.location.href = "/";
+    }, 600);
+});
